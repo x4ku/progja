635d76148ab538bfff5d1a799885f94540a8546f
Zach McAuliffe <zach.mcauliffe@gmail.com>
2022-02-01 23:06:14 -0500

    Remove embedded CSS for card generation
    This feature is not used and is strongly discouraged.

--- progja/decks.py
+++ progja/decks.py
@@ -9,30 +9,30 @@ word_id_pattern = 'progja:word:{ID}'
 sentence_id_pattern = 'progja:sentence:{Sentence}'
 
 
-def create_cards(component, style=None):
+def create_cards(component):
     component_text, component_type = component
     if component_type == 'radical':
-        return create_radical_cards(component, style)
+        return create_radical_cards(component)
     if component_type == 'kanji':
-        return create_kanji_cards(component, style)
+        return create_kanji_cards(component)
     if component_type == 'word':
-        return create_word_cards(component, style)
+        return create_word_cards(component)
     if component_type == 'sentence':
-        return create_sentence_cards(component, style)
+        return create_sentence_cards(component)
     return []
 
 
-def create_radical_cards(component, style=None):
+def create_radical_cards(component):
     component_text, component_type = component
     cards = []
     for radical in kanji.find(component_text):
         if pd.isna(radical['Meaning']):
             continue
-        cards.append(create_radical_card(radical, style))
+        cards.append(create_radical_card(radical))
     return cards
 
 
-def create_radical_card(radical, style=None, id_pattern=radical_id_pattern):
+def create_radical_card(radical, id_pattern=radical_id_pattern):
     return create_card(
         card_id=id_pattern.format(**radical),
         front=[
@@ -53,8 +53,7 @@ def create_radical_card(radical, style=None, id_pattern=radical_id_pattern):
         ],
         tags=create_radical_tags(radical),
         front_classes=['front-radical'],
-        back_classes=['back-radical'],
-        style=style
+        back_classes=['back-radical']
     )
 
 
@@ -62,17 +61,17 @@ def create_radical_tags(radical):
     return create_kanji_tags(radical)
 
 
-def create_kanji_cards(component, style=None):
+def create_kanji_cards(component):
     component_text, component_type = component
     cards = []
     for kanji_ in kanji.find(component_text):
         if pd.isna(kanji_['Meaning']):
             continue
-        cards.append(create_kanji_card(kanji_, style))
+        cards.append(create_kanji_card(kanji_))
     return cards
 
 
-def create_kanji_card(kanji, style=None, id_pattern=kanji_id_pattern):
+def create_kanji_card(kanji, id_pattern=kanji_id_pattern):
     return create_card(
         card_id=id_pattern.format(**kanji),
         front=[
@@ -93,8 +92,7 @@ def create_kanji_card(kanji, style=None, id_pattern=kanji_id_pattern):
         ],
         tags=create_kanji_tags(kanji),
         front_classes=['front-kanji'],
-        back_classes=['back-kanji'],
-        style=style
+        back_classes=['back-kanji']
     )
 
 
@@ -130,16 +128,16 @@ def create_kanji_tags(kanji):
     return sorted(list(tags))
 
 
-def create_word_cards(component, style=None):
+def create_word_cards(component):
     component_text, component_type = component
     found = words.find_common_or_any(component_text)
     cards = []
     for word in found:
-        cards.append(create_word_card(word, style))
+        cards.append(create_word_card(word))
     return cards
 
 
-def create_word_card(word, style=None, id_pattern=word_id_pattern):
+def create_word_card(word, id_pattern=word_id_pattern):
     return create_card(
         card_id=id_pattern.format(**word),
         front=[
@@ -166,8 +164,7 @@ def create_word_card(word, style=None, id_pattern=word_id_pattern):
         ],
         tags=create_word_tags(word),
         front_classes=['front-word'],
-        back_classes=['back-word'],
-        style=style
+        back_classes=['back-word']
     )
 
 
@@ -227,15 +224,15 @@ def create_word_definition_list(definitions):
     return create_ordered_list(items)
 
 
-def create_sentence_cards(component, style=None):
+def create_sentence_cards(component):
     component_text, component_type = component
     cards = []
     for sentence in sentences.find(component_text):
-        cards.append(create_sentence_card(sentence, style))
+        cards.append(create_sentence_card(sentence))
     return cards
 
 
-def create_sentence_card(sentence, style=None, id_pattern=sentence_id_pattern):
+def create_sentence_card(sentence, id_pattern=sentence_id_pattern):
     return create_card(
         card_id=id_pattern.format(**sentence),
         front=create_question_subject(
@@ -262,8 +259,7 @@ def create_sentence_card(sentence, style=None, id_pattern=sentence_id_pattern):
         ],
         tags=create_sentence_tags(sentence),
         front_classes=['front-sentence'],
-        back_classes=['back-sentence'],
-        style=style
+        back_classes=['back-sentence']
     )
 
 
@@ -274,30 +270,23 @@ def create_sentence_tags(sentence):
 
 
 def create_card(
-        card_id, front, back, tags, front_classes=None, back_classes=None,
-        style=None):
+        card_id, front, back, tags, front_classes=None, back_classes=None):
     return {
         'ID': card_id,
-        'Front': create_front(front, front_classes, style),
-        'Back': create_back(back, back_classes, style),
+        'Front': create_front(front, front_classes),
+        'Back': create_back(back, back_classes),
         'Tags': ' '.join(tags)
     }
 
 
-def create_front(contents, front_classes=None, style=None):
+def create_front(contents, front_classes=None):
     front_classes = ['front', *(front_classes or [])]
-    return ''.join([
-        *(create_embedded_style_el(style) if style else []),
-        *create_div(contents, front_classes)
-    ])
+    return ''.join(create_div(contents, front_classes))
 
 
-def create_back(contents, back_classes=None, style=None):
+def create_back(contents, back_classes=None):
     back_classes = ['back', *(back_classes or [])]
-    return ''.join([
-        *(create_embedded_style_el(style) if style else []),
-        *create_div(contents, back_classes)
-    ])
+    return ''.join(create_div(contents, back_classes))
 
 
 def create_question_subject(
@@ -370,10 +359,6 @@ def create_jisho_link(component_text, component_type):
 def create_anchor(content, href, attributes=None):
     attributes = (attributes or {}) | {'href': href}
     return _create_el('a', content, attributes)
-
-
-def create_embedded_style_el(style):
-    return create_el('style', ' '.join([line.strip() for line in style]))
 
 
 def create_div(contents, classes=None):
